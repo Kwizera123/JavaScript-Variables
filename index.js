d6c0bf533209a1c3fc93e2cd2fbd036fd4636acf
Kwizera Theo <kwizerasinapy49@gmail.com>
2023-01-15 07:04:42 +0000

    I changed for addition modifications.

--- index.js
+++ index.js
@@ -7,13 +7,33 @@ console.log(mynaame, year, nighttime, gender);
 console.log("--------------------------------------");
 
 let greeting = "Hello My name is";
-let myname = "Kwizera";
-let lastname = " Sinapy";
-let myfullname = myname+""+lastname;
+let myname = "Kwizera Sinapy";
 let str = "And I have";
 let apples = 10;
-apples += 5;
 let end = "Apples";
-
-console.log(greeting, myfullname, str, apples, end);
+console.log(greeting, myname, str, apples, end);
 console.log("----------------------------------------------------");
+
+let greetin = "Hello My name is";
+let mname = "Kwizera";
+mname = mname + "" + "Sinapy";
+let strgs = "And I have";
+let appls = 10;
+appls = appls + 5;
+let endd = "Apples";
+console.log(greetin, mname, strgs, appls, endd);
+console.log("----------------------------------------------------");
+
+let greetings = "Hello my name is";
+let firstname = "Kwizera";
+let lastname = " Sinapy";
+let fullname = firstname +""+ lastname;
+let strg = "And I have";
+let apple = 10;
+apple += 5; 
+let ends = "apples";
+console.log(greetings, fullname, strg, apple, ends);
+console.log("----------------------------------------------------");
+
+/*Any method used above can be used for every learner.
+Kwizera Theo Sinapy*/
