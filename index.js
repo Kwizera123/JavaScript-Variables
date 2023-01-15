ca796cfe0f024a290ffe0de31624c8f766330590
Sinapy <>
2023-01-15 06:37:06 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+let mynaame = "Kwizera Sinapy";
+let year = 2023;
+let nighttime = false;
+let gender;
+
+console.log(mynaame, year, nighttime, gender);
+console.log("--------------------------------------");
+
+let greeting = "Hello My name is";
+let myname = "Kwizera";
+let lastname = " Sinapy";
+let myfullname = myname+""+lastname;
+let str = "And I have";
+let apples = 10;
+apples += 5;
+let end = "Apples";
+
+console.log(greeting, myfullname, str, apples, end);
+console.log("----------------------------------------------------");
